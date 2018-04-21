2dc87fd03dbae8eff5f38e93b518b226d20ae00b
Daker Zhou🍀 <Daker.Zhou@gmail.com>
2018-04-21 18:17:23 +0800

    配置pm2 生产环境

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,10 @@ module.exports = {
       max_memory_restart: "512M",
       log_date_format: "YYYY-MM-DD HH:mm Z",
       error_file: "/tmp/error.log",
-      out_file: "/tmp/out.log"
+      out_file: "/tmp/out.log",
+      env: {
+        "NODE_ENV": "production",
+      }
     }
   ]
 }
